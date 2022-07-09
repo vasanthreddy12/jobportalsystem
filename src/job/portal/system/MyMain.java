e71bec561b1b58205340bbd7be66e251533058a7
vasanthreddy12 <86564751+vasanthreddy12@users.noreply.github.com>
2022-07-09 11:13:37 +0530

    Add files via upload

---
+++ src/job/portal/system/MyMain.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src.job.portal.system;
+
+/**
+ *  
+ * @author Vasanth Reddy
+ */
+public class MyMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainMenu m = new MainMenu();
+        m.setLocationRelativeTo(null);
+        m.setDefaultCloseOperation(3);
+        m.setVisible(true);
+    }
+}
